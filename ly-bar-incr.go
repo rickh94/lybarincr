16f99e26fa7557c10e2421c004e536d9f947f840
Rick Henry <fredericmhenry@gmail.com>
2018-07-15 02:11:37 -0400

    file is now assembled into byte stream from incremented lines
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- ly-bar-incr.go
+++ ly-bar-incr.go
@@ -10,31 +10,50 @@ import (
 )
 
 func main() {
-	content, err := ioutil.ReadFile("violin_solo_1.ily")
-	if err != nil {
-		log.Fatal(err)
-	}
+	//content, err := ioutil.ReadFile("violin_solo_1.ily")
+	//if err != nil {
+	//  log.Fatal(err)
+	//}
 	//fmt.Printf("File contents: %s", content)
-	lines := strings.SplitAfter(string(content), "\n")
+	lines := readLilypondFile("violin_solo_1.ily")
 	i := 1
 	for _, line := range lines {
 		if 20 < i && i < 80 {
-			fmt.Printf("%d: %v", i, line)
+			fmt.Printf("%d: %v\n", i, line)
 			newLine := incrementBarNum(line, 1)
 			fmt.Println(newLine)
 		}
 		i += 1
 	}
+	newContent := assembleFile(lines, 1, 20, 80)
+	err := ioutil.WriteFile("violin_solo_1_processed.ily", newContent, 0644)
 	//err = ioutil.WriteFile("violin_solo_1_copy.ily", content, 0644)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+// Function readLilypondFile reads the input lilypond file and returns the lines
+func readLilypondFile(path string) []string {
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Println("Could not open file")
+		log.Fatal(err)
+	}
+	backupPath := strings.Join([]string{path, ".bak"}, "")
+	err = ioutil.WriteFile(backupPath, content, 644)
+	if err != nil {
+		fmt.Println("Could not write backup")
+		log.Fatal(err)
+	}
+	return strings.Split(string(content), "\n")
+}
+
 // Function incr_bar_num takes a line and increment value and returns the line with the
 // bar number incremented to that value
 func incrementBarNum(line string, increment int64) string {
-	//var newLine string
+	// select the delimiter to split on depending on whether the bar number is
+	// a check or comment
 	var delimiter string
 	if strings.Contains(line, "\\barNumberCheck #") {
 		delimiter = "#"
@@ -43,7 +62,9 @@ func incrementBarNum(line string, increment int64) string {
 	} else {
 		return line
 	}
-	parts := strings.SplitAfter(strings.Trim(line, "\n"), delimiter)
+
+	// split the line, convert and increment the number
+	parts := strings.SplitAfter(line, delimiter)
 	numAsString := parts[len(parts)-1]
 	extraSpace := "" // if a space is stripped, a space needs to be added back in, if not, not
 	if strings.Contains(numAsString, " ") {
@@ -55,6 +76,26 @@ func incrementBarNum(line string, increment int64) string {
 		log.Fatal(err)
 	}
 	newBarNum := barNum + increment
-	parts = append(parts[:len(parts)-1], extraSpace, strconv.FormatInt(newBarNum, 10), "\n")
+
+	// reassemble the line into a slice
+	parts = append(parts[:len(parts)-1], extraSpace, strconv.FormatInt(newBarNum, 10))
+	// join the slice and return the new line
 	return strings.Join(parts, "")
+}
+
+// Function assembleFile takes the lines from a file, the increment, and line number range,
+// increments the appropriate lines, and then returns the new file bytes
+func assembleFile(lines []string, increment int64, firstLine int, lastLine int) []byte {
+	var newLines []string
+	i := 1
+	for _, line := range lines {
+		if i >= firstLine && i <= lastLine {
+			newLines = append(newLines, incrementBarNum(line, increment))
+		} else {
+			newLines = append(newLines, line)
+		}
+		i += 1
+	}
+	newLinesStream := strings.Join(newLines, "\n")
+	return []byte(newLinesStream)
 }
